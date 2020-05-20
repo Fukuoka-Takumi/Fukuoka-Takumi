9f4b6e5fb75985b117e49c965805fb02af0a5aac
Fukuoka-Takumi <https://github.com/Fukuoka-Takumi/Fukuoka-Takumi.git>
2020-05-20 18:48:36 +0900

    課題提出 - 変数・定数

---
+++ variable/src/variable/Task3.java
@@ -0,0 +1,25 @@
+package variable;
+
+public class Task3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		System.out.println("変換前");
+		int intNum = 219;
+		System.out.println(intNum);
+		double doubleNum = 364.2;
+		System.out.println(doubleNum);
+		long longNum = 123456L;
+		System.out.println(longNum);
+		System.out.println("変換後");
+		long x =(long)intNum;
+		System.out.println(x);
+		float y =(float)doubleNum;
+		System.out.println(y);
+		double z =(double)longNum;
+		System.out.println(z);
+
+	}
+
+}
