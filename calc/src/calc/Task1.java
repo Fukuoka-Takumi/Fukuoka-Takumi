5b7b5ee00019193d86820593d03d9402fb04d4bf
Fukuoka-Takumi <https://github.com/Fukuoka-Takumi/Fukuoka-Takumi.git>
2020-05-21 15:57:51 +0900

    課題提出 - 四則演算

---
+++ calc/src/calc/Task1.java
@@ -0,0 +1,19 @@
+package calc;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 3, b = 2, c = 6, d= 5;
+		System.out.println("修正前:" + (a + b * c + d));
+		System.out.println("修正後:" + ((a + b) * c + d));
+		
+		int e;
+		e = 25;
+		int f;
+		f =3;
+		System.out.println ((e)+("/")+(f)+("の商") + (e/f) + (": 余り")+ (e%f));
+
+	}
+
+}
