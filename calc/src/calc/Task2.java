ee47d54e9307022705edc065022859f814f007f1
Fukuoka-Takumi <https://github.com/Fukuoka-Takumi/Fukuoka-Takumi.git>
2020-05-21 16:21:26 +0900

    課題提出 - 四則演算

---
+++ calc/src/calc/Task2.java
@@ -0,0 +1,14 @@
+package calc;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        int a = 10, b = 12, c = 13, d = 3;
+        double e = 3.0;
+        System.out.println("合計:"+(a+b+c));
+        System.out.println("平均"+("(")+(d)+("で割った時):")+(a+b+c)/d);
+        System.out.println("平均"+("(")+(e)+("で割った時):")+(a+b+c)/e);
+	}
+
+}
