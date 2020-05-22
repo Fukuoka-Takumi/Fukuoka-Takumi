3d87cc16595d9428d392ce5d4c655aa763b4a2f5
Fukuoka-Takumi <https://github.com/Fukuoka-Takumi/Fukuoka-Takumi.git>
2020-05-22 11:55:51 +0900

    再提出 -変数・定数

--- variable/src/variable/Task1.java
+++ variable/src/variable/Task1.java
@@ -5,26 +5,19 @@ public class Task1 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 double a;
-a = 31.2;
-System.out.println(a);
-
+ a = 31.2;
+  System.out.println(a);
 String b;
-b = "Javaの基礎";
-System.out.println(b);
-
+ b = "Javaの基礎";
+  System.out.println(b);
 int c;
-c = 9;
-System.out.println(c);
-
+ c = 9;
+  System.out.println(c);
 long d;
-d = 2147483648L;
-System.out.println(d);
-
+ d = 2147483648L;
+  System.out.println(d);
 char e;
-e = '才';
-System.out.println(e);
-
-
+ e = '才';
+  System.out.println(e);
 	}
-
 }
--- variable/src/variable/Task2.java
+++ variable/src/variable/Task2.java
@@ -5,10 +5,9 @@ public class Task2 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		String name = "山田　太郎";
-		String number ="'090-9999-1111'";
-
+		final String TEISU1 ="'090-9999-1111'";
         System.out.println(name + "です。");
-        System.out.println("私の電話番号は"+ number + "です。");
+        System.out.println("私の電話番号は"+ TEISU1 + "です。");
 	}
 
 }
