839617205ec3cefc3aba07eaf303c02190fad87b
Fukuoka-Takumi <https://github.com/Fukuoka-Takumi/Fukuoka-Takumi.git>
2020-05-22 17:08:37 +0900

    課題提出 - 条件分岐

---
+++ branch/src/branch/Task3.java
@@ -0,0 +1,21 @@
+package branch;
+
+import java.util.Scanner;
+
+public class Task3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner sc = new Scanner(System.in);
+		
+		int data = sc.nextInt();
+		
+		if(data>0)
+		 System.out.println("入力された数字は正の数です。");
+		if(data<0)
+		 System.out.println("入力された数字は負の数です。");
+		if(data == 0)
+		 System.out.println("入力された数字は0です。");
+		sc.close();
+	}
+}
