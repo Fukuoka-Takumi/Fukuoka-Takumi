8490c7774d266ba6032bd0eb46c0cdc4441477b9
Fukuoka-Takumi <https://github.com/Fukuoka-Takumi/Fukuoka-Takumi.git>
2020-05-22 15:02:02 +0900

    課題提出 - 四則演算

---
+++ calc/src/calc/Task5.java
@@ -0,0 +1,25 @@
+package calc;
+
+import java.util.Scanner;
+
+public class Task5 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String a =("ノートPCの値段:");
+		final int b =(89800);
+		char c =('円');
+		
+		System.out.println(a+b+c);
+		
+		Scanner sc = new Scanner(System.in);
+		String d = sc.nextLine();
+		String e = sc.nextLine();
+		sc.close();
+		
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(d+e);
+	}
+
+}
