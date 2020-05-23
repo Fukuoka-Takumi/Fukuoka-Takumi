432ee0fa380f6a1a90746e9b08b404cb17d6d0af
Fukuoka-Takumi <https://github.com/Fukuoka-Takumi/Fukuoka-Takumi.git>
2020-05-23 16:38:14 +0900

    再提出 -変数・定数

--- variable/src/variable/Task2.java
+++ variable/src/variable/Task2.java
@@ -3,11 +3,9 @@ package variable;
 public class Task2 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		String name = "山田　太郎";
+		String name = "山田 太郎";
 		final String TEISU1 ="'090-9999-1111'";
-        System.out.println(name + "です。");
-        System.out.println("私の電話番号は"+ TEISU1 + "です。");
+		System.out.println(name + "です。");
+		System.out.println("私の電話番号は"+ TEISU1 + "です。");
 	}
-
 }
