cabfdad9650db7a4c22c55fe825c0c5a6724ae8a
Fukuoka-Takumi <https://github.com/Fukuoka-Takumi/Fukuoka-Takumi.git>
2020-05-23 14:46:08 +0900

    再提出 - Javaの基礎

--- basic/src/basic/Task1.java
+++ basic/src/basic/Task1.java
@@ -2,8 +2,6 @@ package basic;
 
 public class Task1 {
 	public static void main(String[] args) {
-		System.out.println("初めての課題");
-		
-		System.out.println("攻略の道");
-	}
+		System.out.println("初めての課題\n攻略の道");
+}
 }
--- basic/src/basic/Task2.java
+++ basic/src/basic/Task2.java
@@ -4,9 +4,10 @@ public class Task2 {
 
 	public static void main(String[] args) {
 		/*
+		コメント
+		コメント
+		 */
 		System.out.println("hogehoge");
 		System.out.println("hogehoge2");
-		*/
-	}
-
+}
 }
