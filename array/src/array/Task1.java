f5572e8a04ccbbfd90c66f8cfa8fd91f38752fde
Fukuoka-Takumi <https://github.com/Fukuoka-Takumi/Fukuoka-Takumi.git>
2020-05-30 15:08:14 +0900

    再提出 - 配列

--- array/src/array/Task1.java
+++ array/src/array/Task1.java
@@ -3,13 +3,13 @@ package array;
 public class Task1 {
 
 	public static void main(String[] args) {
-		int[] array = {4,9,19,5,8,21,42,64,73,18,2};
+		int[] array = {4, 9, 19, 5, 8, 21, 42, 64, 73, 18, 2};
 		System.out.print("配列: [");
 		for(int i = 0; i < array.length; i++) {
-			if(array[i] > array[10]) {
+			if(i != array.length - 1) {
 				System.out.print(array[i] + ",");
-			}else {
-				System.out.println(array[10] + "]");
+			}else{
+				System.out.println(array[i] + "]");
 			}
 		}
 		int sum = 0;
--- array/src/array/Task2.java
+++ array/src/array/Task2.java
@@ -14,7 +14,7 @@ public class Task2 {
 				System.out.print(array[i] + ",");
 			}
 		}
-		for(int i = array.length-1; i >= 0; i--) {
+		for(int i = array.length - 1; i >= 0; i--) {
 			if(i == 0) {
 				System.out.println(array[i]);
 			}else {
