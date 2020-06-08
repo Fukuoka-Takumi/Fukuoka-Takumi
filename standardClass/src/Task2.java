5bb5414d053031064d2f0713d868e6c06e1e690f
Fukuoka-Takumi <https://github.com/Fukuoka-Takumi/Fukuoka-Takumi.git>
2020-06-08 17:41:11 +0900

    課題提出 - 標準クラス

---
+++ standardClass/src/Task2.java
@@ -0,0 +1,13 @@
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		String str = "Java Programming Training";
+		String subStr1 = str.substring(17);
+		String subStr2 = str.substring(0, 15);
+
+		System.out.println(subStr1);
+		System.out.println(subStr2);
+	}
+
+}
