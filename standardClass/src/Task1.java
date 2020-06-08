668c416a5f3997b228f6f0d6938a39a18c905f71
Fukuoka-Takumi <https://github.com/Fukuoka-Takumi/Fukuoka-Takumi.git>
2020-06-08 17:15:21 +0900

    課題提出 - 標準クラス

---
+++ standardClass/src/Task1.java
@@ -0,0 +1,13 @@
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		String str1 = " tech-compass ";
+		String str2 = str1.trim();
+		String upper_str1 = str2.toUpperCase();
+
+		System.out.println("変換前: " + str1);
+		System.out.println("変換後: " + upper_str1);
+	}
+
+}
