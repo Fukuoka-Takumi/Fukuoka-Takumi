941c8e1e201d1d3c81334516ebcedb81d787f0cc
Fukuoka-Takumi <https://github.com/Fukuoka-Takumi/Fukuoka-Takumi.git>
2020-06-08 16:05:53 +0900

    再提出 - メソッド

--- method/src/method/Task1.java
+++ method/src/method/Task1.java
@@ -3,13 +3,11 @@ package method;
 public class Task1 {
 
 	public static void main(String[] args) {
-		myProfile("コラボ");
-		myProfile(30,178.0);
+		myProfile("コラボ", 30, 178.0);
 	}
-	public static void myProfile(String name) {
+
+	public static void myProfile(String name, int age, double se) {
 		System.out.println("私の名前は" + name + "です。");
-	}
-	public static void myProfile(int age, double se) {
 		System.out.println("年齢は" + age + "歳です。");
 		System.out.println("身長は" + se + "cmです。");
 		System.out.println("よろしくお願いします");
--- method/src/method/Task2.java
+++ method/src/method/Task2.java
@@ -4,12 +4,11 @@ public class Task2 {
 
 	public static void main(String[] args) {
 		System.out.println("三角形の面積");
-		double Area = getTriangleArea(135.0, 2.0); //高さ,底辺
-		System.out.println(Area); //面積値
+ //高さ,底辺 修正8行目に入れた ローワーキャメル
+		System.out.println(getTriangleArea(135.0, 2.0)); //面積値
 	}
 	public static double getTriangleArea(double takasa, double tei) {
-		double Area = takasa / tei * 2; //三角形の面積
-		return Area; //結果を上に戻す
+		return Math.round(takasa / tei * 2); //三角形の面積 上に戻す math.roundで四捨五入
 	}
 
 }
